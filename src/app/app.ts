6d905623ed3a0b330981158fd707ff24ef45bb3f
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-22 08:13:44 -0300

    Criado primeiro component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from './title/title';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Title],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
